fbc139b52467d643eb99c01a4eea88645e3ef64c
Vaibav Bajra S <svaibavbajra@gmail.com>
2021-09-20 07:32:19 +0530

    plyaer screen2

--- client/pages/playerScreen2.js
+++ client/pages/playerScreen2.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 // import styles from "../css/hostScreen.module.css"
 
-const hostScreen10 = () => {
+const playerScreen2 = () => {
 
     const router = useRouter()
 
@@ -57,38 +57,17 @@ const hostScreen10 = () => {
         <div className="flex flex-row justify-center" style={{height:"100vh"}}>
             <SettingsAndBack />
             <div className="flex flex-column justify-evenly">
-                <SendCodeToInvitePlayers gameCode={gameCode} numberOfPlayers={numberOfPlayers}/>
+                <div className="text-center font-bold text-xl">
+                    Choose a Team
+                </div>
 
                 <div className="flex flex-row justify-evenly w-screen">
                     <div className="bg-gray-200">
                         <div className="font-bold text-xl mx-4 mt-4 flex justify-between">
-                            <div>Players Online</div>
-                            <div className="flex">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                </svg>
-                                <div className="ml-2 text-base">New Team</div>
-                            </div>
-                        </div>
-                        <div className="font-bold mx-4 my-4">
-                            20 Players
-                        </div> 
-                        <div className="flex flex-row flex-wrap max-w-sm max-h-44 justify-evenly my-4" style={{overflowY:"auto"}} id="players">
-                            {players.map(player => (
-                                <div style={{zIndex:2, textAlign:"center"}} onClick={event => deletePlayer({x: event.clientX, y: event.clientY, player: player})}>
-                                    <div className="mx-7">
-                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                        </svg>
-                                    </div>
-                                    {player.name}
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                    <div className="bg-gray-200">
-                        <div className="font-bold text-xl mx-4 mt-4">
-                            TeamName
+                            <div>TeamName</div>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
                         </div>
                         <div className="font-bold mx-4 my-4">
                             4 Players
@@ -107,8 +86,34 @@ const hostScreen10 = () => {
                         </div>
                     </div>
                     <div className="bg-gray-200">
-                        <div className="font-bold text-xl mx-4 mt-4">
-                            TeamName
+                        <div className="font-bold text-xl mx-4 mt-4 flex justify-between">
+                            <div>TeamName</div>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+                        </div>
+                        <div className="font-bold mx-4 my-4">
+                            4 Players
+                        </div> 
+                        <div className="flex flex-row flex-wrap max-w-xs max-h-44 justify-evenly my-4" style={{overflowY:"auto"}} id="players">
+                            {players.map(player => (
+                                <div style={{zIndex:2, textAlign:"center"}} onClick={event => deletePlayer({x: event.clientX, y: event.clientY, player: player})}>
+                                    <div className="mx-7">
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-16 w-16" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                        </svg>
+                                    </div>
+                                    {player.name}
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="bg-gray-200">
+                        <div className="font-bold text-xl mx-4 mt-4 flex justify-between">
+                            <div>TeamName</div>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
                         </div>
                         <div className="font-bold mx-4 my-4">
                             4 Players
@@ -136,7 +141,7 @@ const hostScreen10 = () => {
 
                 </div>
 
-                <div className="text-center"><button onClick={() => router.push("/hostScreen8")} className="bg-gray-200 border-2 border-black rounded-md px-4 py-2 text-xl font-bold">Start</button></div>
+                <div className="text-center px-4 py-2 text-xl font-bold">Host will start the game</div>
 
 
                 {
@@ -150,4 +155,4 @@ const hostScreen10 = () => {
      );
 }
  
-export default hostScreen10;
+export default playerScreen2;

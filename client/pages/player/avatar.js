08b2720b32e1524b925b120ff1168aa9454b4379
Vaibav Bajra S <svaibavbajra@gmail.com>
2021-09-23 17:07:40 +0530

    avatar slider

--- client/pages/player/avatar.js
+++ client/pages/player/avatar.js
@@ -6,6 +6,9 @@ const avatar = () => {
 
     const socket  = useContext(SocketContext)
     const [numberOfPlayers, setNumberOfPlayers] = useState(0)
+    
+    const [color, setColor] = useState(0)
+    const colors = ["#f0f", "#ff0", "#0ff","#f00","#0f0", "#00f"]
 
     const [players, setPlayers] = useState([])
 
@@ -27,13 +30,13 @@ const avatar = () => {
                             Choose your avatar
                         </div>
                         <div className="flex flex-row justify-evenly items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor" onClick={() => setColor((color-1)%6)}>
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7" />
                             </svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-40 w-40" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                            </svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-40 w-40" fill="none" viewBox="0 0 24 24" stroke={colors[color]}>
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor" onClick={() => setColor((color+1)%6)}>
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7" />
                             </svg>
                         </div>

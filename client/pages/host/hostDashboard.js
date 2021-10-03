578bc8d347879145fe384b9fb880dec109909021
Vaibav Bajra S <svaibavbajra@gmail.com>
2021-10-03 14:48:53 +0530

    host dashboard completed

--- client/pages/host/hostDashboard.js
+++ client/pages/host/hostDashboard.js
@@ -15,10 +15,11 @@ const hostDashboard = () => {
     const [disableRounds, setDisableRounds] = useState(true)
 
     const [selected, setSelected] = useState("teams")
+    const [ruleBook, ruleBookClicked] = useState(false)
     const [playersWithoutTeams, setPlayers] = useState([{name: "Player 1"}, {name: "Player 2"}, {name: "Player 3"}, {name: "Player 4"}, {name: "Player 5"}, {name: "Player 6"}])
     const [teams, setTeams] = useState([{teamName:"Team 01", call:true}, {teamName:"Team 02"}, {teamName:"Team 03"}])
     const [players, setPlayer] = useState(["Player 1", "Player 2", "Player 3", "Player 4", "Player 5"])
-    const [roundResults, setRoundResult] = useState(["Angry", "Happy", "Annoyed", "Excited", "COol", "", "", "", "", ""])
+    const [roundResults, setRoundResult] = useState(["Angry", "Happy", "Annoyed", "Excited", "Cool", "", "", "", "", ""])
 
     useEffect(() => {}, [])
 
@@ -49,10 +50,10 @@ const hostDashboard = () => {
                 <div onClick={() => setSelected("lobby")} className={selected==="lobby"?"bg-gray-400 w-full text-center py-4 cursor-pointer":"w-full cursor-pointer p-4 text-center"}>Lobby</div>
             </div>
             <div className="mb-8">
-                <span className="px-4 bg-red-400 py-2">END GAME</span>
+                <span className="px-4 bg-red-400 py-2 cursor-pointer">END GAME</span>
             </div>
         </div>
-        <div className="px-4 py-2 bg-gray-200 text-lg font-bold" style={{position:"absolute", top:"2rem", right:"2rem"}}>
+        <div className="px-4 py-2 bg-gray-200 text-lg font-bold cursor-pointer" style={{position:"absolute", top:"2rem", right:"2rem"}} onClick={() => ruleBookClicked(true)}>
             Rule Book
         </div>  
         <div style={{flex:9}}>
@@ -248,6 +249,14 @@ const hostDashboard = () => {
                 <PlayerComponent players = {playersWithoutTeams} teams = {teams} />
             </div>}
         </div>
+
+{/* Rule book */}
+        {ruleBook?<div className="flex justify-center h-screen w-screen bg-black bg-opacity-50 overflow-hidden items-center" style={{position:"absolute", top:"0", left:"0"}}>
+            <div className="bg-red-200 h-4/5 w-4/5 relative">
+                <div className="text-3xl cursor-pointer" style={{position:"absolute", top:"2rem", right:"2rem"}} onClick={() => ruleBookClicked(false)}>&times;</div>
+                <div className="text-center font-bold text-2xl mt-4">RULE BOOK</div>
+            </div>
+        </div>:<></>}
     </div> );
 }
  

abc36f86913198aa9bc50c09c8c7cfac962bb26f
Manan shah <sc19mas@leeds.ac.uk>
2021-10-09 16:00:07 +0100

    server changes

--- client/context/socket/SocketContext.js
+++ client/context/socket/SocketContext.js
@@ -1,4 +1,4 @@
 import socketio from "socket.io-client";
 import React from "react";
-export const socket = socketio.connect('https://emo-g-backend.herokuapp.com/');
+export const socket = socketio.connect('https://floating-ocean-39302.herokuapp.com/');
 export const SocketContext = React.createContext();

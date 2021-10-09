3201efc436a1dbd3638d983b0fdbdf57fb4f6747
Manan shah <sc19mas@leeds.ac.uk>
2021-10-09 14:34:45 +0100

    pushing to vercel

--- client/context/socket/SocketContext.js
+++ client/context/socket/SocketContext.js
@@ -1,4 +1,4 @@
 import socketio from "socket.io-client";
 import React from "react";
-export const socket = socketio.connect('https://emoji-manan.herokuapp.com/');
+export const socket = socketio.connect('https://emo-g-backend.herokuapp.com/');
 export const SocketContext = React.createContext();

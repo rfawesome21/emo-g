afb84d1410af9fe5303243a4db36780d54bdac26
Manan shah <sc19mas@leeds.ac.uk>
2021-10-09 15:49:53 +0100

    heroku push

--- server/server.js
+++ server/server.js
@@ -2,16 +2,20 @@
 const express = require('express')
 const cors = require('cors')
 const colors = require('colors')
+const http = require('http')
 
 const app = express()
 app.use(express.json())
 
 app.use(cors())
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 9000
 const MODE = process.env.MODE || 'development'
 
-const io = require('socket.io')(9000, {
+
+const httpServer=http.createServer(app)
+
+const io = require('socket.io')(httpServer, {
     cors: {
       origins: ['http://localhost:3000', '*'],
     },
@@ -19,25 +23,19 @@ const io = require('socket.io')(9000, {
 
 const gameSetup = require('./game/GameSetup')
 const gameAuth = require('./game/GameAuth')
-const gameTeams = require('./game/GameTeams')
-const gameScenes = require('./game/GameScenes')
-const gamePlay = require('./game/GamePlay')
-
+  
 const onConnection = socket => {
   gameSetup(io, socket)
   gameAuth(io, socket)
-  gameScenes(io, socket)
-  gameTeams(io, socket)
-  gamePlay(io, socket)
 }
 
 io.on(
    'connection', onConnection 
 )
 
-app.listen(
-    PORT,
-    console.log(
-      `Server running on port ${PORT} in ${MODE.rainbow.bold + ' mode'.yellow.bold} `.yellow.bold
-    )
+httpServer.listen(
+  PORT,
+  console.log(
+    `Server running on port ${PORT} in ${MODE.rainbow.bold + ' mode'.yellow.bold} `.yellow.bold
+  )
 )

da2cbe2445f781b116053e70c42130ec3d343ad3
Manan shah <sc19mas@leeds.ac.uk>
2021-10-17 15:08:56 +0100

    back button displayed only to host now

--- client/components/settingsAndBack.js
+++ client/components/settingsAndBack.js
@@ -15,7 +15,7 @@ const SettingsAndBack = ({link, player}) => {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>
             </div>
-            {player?<Link href = {link}>
+            {!player?<Link href = {link}>
             <div style={{position:"absolute", left:"2rem", top:"2rem", cursor:"pointer"}}>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                     < path fillRule="evenodd" d="M9.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L7.414 9H15a1 1 0 110 2H7.414l2.293 2.293a1 1 0 010 1.414z" clipRule="evenodd" />

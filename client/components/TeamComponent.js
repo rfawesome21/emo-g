5695b29238a5b52deb8b242932974d96fcf40059
Manan shah <sc19mas@leeds.ac.uk>
2021-10-19 01:21:31 +0100

    host can now join player screen

--- client/components/TeamComponent.js
+++ client/components/TeamComponent.js
@@ -51,7 +51,7 @@ const TeamComponent = ({teams, activeIcon, player, playersWithoutTeams}) => {
                 </div>
                 {!player?<div className="flex" onClick = {createNewTeam}>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
                     <div className="ml-2 text-base">New Team</div>
                 </div>:<></>}
--- client/pages/player/game/[teamName].js
+++ client/pages/player/game/[teamName].js
@@ -4,6 +4,7 @@ import ExitGame from "../../../components/exitGame";
 import { SocketContext } from "../../../context/socket/SocketContext";
 import Button from '../../../components/Button'
 import Wheel from '../../../components/wheel'
+import SettingsAndBack from "../../../components/settingsAndBack";
 const game = () => {
 
     const router = useRouter()
@@ -33,10 +34,11 @@ const game = () => {
     const [gameCode, setGameCode] = useState('')
     const timerRef = useRef()
     const [currentRoundEmotion, setCurrentRoundEmotion] = useState('')
+    const [status, setStatus] = useState('')
 
     useEffect(() => {
+        setStatus(sessionStorage.getItem('status'))
         setPlayerName(sessionStorage.getItem('player-name'))
-        const teamName = sessionStorage.getItem('team-name')
         const code = sessionStorage.getItem('game-code')
         setGameCode(sessionStorage.getItem('game-code'))        
         socket.emit('join-team-room', {code, teamName })
@@ -177,6 +179,7 @@ const game = () => {
 
     return ( 
         <div className="flex flex-column h-screen">
+            {status === '1' ? <SettingsAndBack link = '/host/hostDashboard' /> : null}
             <div className="flex justify-end my-8">
                 <div className="bg-gray-200 font-bold py-2 px-4 mx-2 cursor-pointer">
                     Call host
@@ -185,12 +188,12 @@ const game = () => {
                     Rule Book
                 </div>
                 <div className="py-2 px-4 cursor-pointer mr-8">
-                    <div onClick={() => setSettingsPressed(true)}>
+                    {status !== '1'?<div onClick={() => setSettingsPressed(true)}>
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                        </svg>
-                    </div>
+                        </svg> 
+                    </div> : null}
                 </div>
             </div>
             <div className="flex flex-row px-8 pb-4" style={{flex:"1"}}>

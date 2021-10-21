2123057c0b94d2b6bdb972f6fde3db395933e5e4
Manan shah <sc19mas@leeds.ac.uk>
2021-10-21 12:16:42 +0100

    mirror push

--- client/components/sendCodeToInvitePlayers.js
+++ client/components/sendCodeToInvitePlayers.js
@@ -38,7 +38,7 @@ const sendCodeToInvitePlayers = (props) => {
                         </svg>
                     </span>
                 </div>
-                <div className="border-2 py-2 px-4 border-black rounded-md font-bold text-xl">
+                <div className="border-2 py-2 px-4 border-black rounded-md font-bold text-lg">
                     {props.numberOfPlayers} Joined
                 </div>
             </div>

a258794113d352e5492c55269396ba2ae541dfc3
Vaibav Bajra S <svaibavbajra@gmail.com>
2021-10-22 18:55:50 +0530

    host/settings/

---
+++ client/components/endGame.js
@@ -0,0 +1,9 @@
+const EndGame = (props) => {
+    return ( 
+        <div className="absolute bottom-12 cursor-pointer font-bold left-12 endGame px-4 py-2">
+            End Game
+        </div>
+     );
+}
+ 
+export default EndGame;
--- client/pages/host/settings.js
+++ client/pages/host/settings.js
@@ -4,6 +4,7 @@ import SendCodeToInvitePlayers from "../../components/sendCodeToInvitePlayers"
 import SettingsAndBack from "../../components/settingsAndBack"
 import styles from '../../styles/Settings.module.css'
 import { SocketContext } from "../../context/socket/SocketContext"
+import EndGame from "../../components/endGame"
 
 const settings = () => {
 
@@ -73,90 +74,80 @@ const settings = () => {
             {/* <SettingsAndBack link = '/play' /> */}
             <div className="flex flex-column justify-evenly">
                 <SendCodeToInvitePlayers gameCode={gameCode} numberOfPlayers={numberOfPlayers}/>
-                <div className="flex flex-row justify-between bg-gray-200 px-4 py-4">
-                    <div className="pr-4  font-bold text-xl">Set Timer</div>
-                    <div className="px-4 text-xl"><input type="radio" name="mode" defaultChecked onClick = {() => setDisabled(true)} /> Default</div>
-                    <div className="pl-4">
-                        <div className="text-xl"><input type="radio" name="mode" onClick = {() => setDisabled(false)} /> Manual</div>
-                        <div className="flex justify-between ml-4 my-2 text-lg">
-                            Guessing time 
-                            <input type="number"
-                            min="1"
-                            max="10"
-                            onChange = {e => onChangeHandlerInMinutes(e)}
-                            value = {guessingTime}
-                            disabled={disabled? true:false}
-                            className={disabled?`ml-1 text-center disabled:opacity-50 bg-gray-500 w-14 text-blue-100` : 'ml-1 text-center w-14'}
-                            name = "guess"
-                            />
-                            <input type = "number"
-                            min="1"
-                            max="60"
-                            className={disabled?`ml-1 text-center disabled:opacity-50 bg-gray-500 w-14 text-blue-100` : 'ml-1 text-center w-14'}
-                            onChange = {e => onChangeHandlerInSeconds(e)}
-                            value = {guessingTimeInSeconds}
-                            disabled={disabled? true:false}
-                            name = "guessInSeconds"
-                            />
-                            mins
+                <div className="heading w-80 px-8 py-8 rounded-2xl">
+                    <div className="pr-4 font-bold text-xl">Set Timer</div>
+                    <br/>
+                    <div className="container-fluid my-2 text-lg">
+                        <div className="row">
+                            <div className="col-6 p-0">Guessing time</div>
+                            <div className="col-6 p-0">
+                                <input type="number"
+                                min="1"
+                                max="10"
+                                onChange = {e => onChangeHandlerInMinutes(e)}
+                                value = {guessingTime}
+                                className={`ml-1 text-center w-14 counterInput`}
+                                name = "guess"
+                                />
+                                <input type = "number"
+                                min="1"
+                                max="60"
+                                className={`ml-1 text-center w-14 counterInput`}
+                                onChange = {e => onChangeHandlerInSeconds(e)}
+                                value = {guessingTimeInSeconds}
+                                name = "guessInSeconds"
+                                />
+                            </div>
                         </div>
-                        <div className="flex justify-between ml-4 my-2 text-lg">
-                            Typing time 
-                            <input 
-                            type="number" 
-                            min='1' 
-                            max='10'
-                            value={typingTime} 
-                            className={disabled?`ml-6 text-center disabled:opacity-50 bg-gray-500 w-14 text-blue-100` : 'ml-6 text-center w-14'}
-                            disabled={disabled? true:false}
-                            name = 'type'
-                            onChange = {e => onChangeHandlerInMinutes(e)}
-                            />
-                            <input 
-                            type="number" 
-                            min='1' 
-                            max='60'
-                            value={typingTimeInSeconds} 
-                            className={disabled?`ml-1 text-center disabled:opacity-50 bg-gray-500 w-14 text-blue-100` : 'ml-1 text-center w-14'}
-                            disabled={disabled? true:false}
-                            name = 'type'
-                            onChange = {e => onChangeHandlerInSeconds(e)}
-                            />
-                            mins
+                    </div>
+                    <div className="my-2 text-lg container-fluid">
+                        <div className="row">
+                            <div className="col-6 p-0">Typing time</div>
+                            <div className="col-6 p-0">
+                                <input 
+                                type="number" 
+                                min='1' 
+                                max='10'
+                                value={typingTime} 
+                                className={`ml-1 text-center w-14 counterInput`}
+                                name = 'type'
+                                onChange = {e => onChangeHandlerInMinutes(e)}
+                                />
+                                <input 
+                                type="number" 
+                                min='1' 
+                                max='60'
+                                value={typingTimeInSeconds} 
+                                className={`ml-1 text-center w-14 counterInput`}
+                                name = 'type'
+                                onChange = {e => onChangeHandlerInSeconds(e)}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <br/>
+                    <div className="font-bold text-xl">Rounds</div>
+                    <br />
+                    <div className="container-fluid">
+                        <div className="row">
+                            <div className="col-6 text-lg p-0">
+                                No. of rounds
+                            </div>
+                            <div className="col-6 p-0">
+                                <input 
+                                value={numberOfRounds} 
+                                onChange={event => setNumberOfRounds(event.target.value)} 
+                                type="number" min="6" 
+                                placeholder = 'Set Number of Rounds' 
+                                className={'ml-1 text-center h-7 w-14 counterInput'}
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-row justify-between bg-gray-200 px-4 py-4">
-                    <div className="font-bold text-xl">Number of rounds</div>
-                    <div className="text-xl">
-                        <input type="radio" 
-                        name="round" 
-                        className="form-checkbox"
-                        defaultChecked
-                        onClick = {() => setDisableRounds(true)}
-                        /> 
-                        10 Rounds(Default)
-                        </div>
-                    <div className="text-xl">
-                        <input 
-                        type="radio" 
-                        name="round" 
-                        className="form-checkbox"
-                        onClick = {() => setDisableRounds(false)}
-                        /> 
-                        <input 
-                        value={numberOfRounds} 
-                        onChange={event => setNumberOfRounds(event.target.value)} 
-                        type="number" min="6" 
-                        style={{width:"10rem"}}
-                        placeholder = 'Set Number of Rounds' 
-                        disabled={disableRounds?true:false}
-                        className={disableRounds? 'border-2 bg-gray-300 text-black text-sm' : 'border-2 text-sm'}
-                        />
-                    </div>
-                </div>
-                <div className="text-center"><button onClick={continueGame} className="bg-gray-200 border-2 border-black rounded-md px-4 py-2 text-xl font-bold">Continue</button></div>
+                <div className="text-center"><button onClick={continueGame} className="buttonNew rounded-md px-4 py-2 text-xl font-bold">Continue</button></div>
             </div>
+            <EndGame />
         </div>
      );
 }

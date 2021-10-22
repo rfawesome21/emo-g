2c36c4fffb511c05e98c382ce2f665ccd46492e5
Vaibav Bajra S <svaibavbajra@gmail.com>
2021-10-22 19:19:43 +0530

    lifeline popups

--- client/components/Players/confirmLifeline.js
+++ client/components/Players/confirmLifeline.js
@@ -2,13 +2,14 @@ const ConfirmLifeline = (props) => {
     console.log(props);
     return ( 
         <div className="h-screen w-screen bg-opacity-50 absolute top-0 left-0 flex justify-center items-center" style={{backgroundColor:"rgba(235, 162, 130, 0.5)"}}>
-            <div className="bg-gray-200 rounded-lg p-4 text-center" style={{backgroundColor:"#fffaee"}} onClick={() => props.setConfirmLifeline(false)}>
-               <div>
+            <div className="bg-gray-200 rounded-lg p-4 text-center" style={{backgroundColor:"#fffaee", color:"#da764b"}}>
+               <div className="text-xl font-bold">
                     Do you want to use the lifeline<br />{props.lifeLine}?
                </div>
+               <div className="my-2">This will do some shit</div>
                <div className="flex justify-evenly items-center">
-                    <div>Yes</div>
-                    <div>No</div>
+                    <div className="buttonNew text-lg px-2 py-0 rounded" onClick={() => {}}>Yes</div>
+                    <div className="buttonNew text-lg px-2 py-0 rounded" onClick={() => props.setConfirmLifeline(false)}>No</div>
                </div>
             </div>                
         </div>

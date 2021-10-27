60196e5b42590fcd4cbdba3450ae4ea6da7ae220
Vaibav Bajra S <svaibavbajra@gmail.com>
2021-10-27 22:17:17 +0530

    wheel v3

--- client/pages/player/game/[teamName].js
+++ client/pages/player/game/[teamName].js
@@ -195,6 +195,10 @@ const game = () => {
     }
 
     const clickHandler = () => {
+        setDeletedRow([])
+        setOtherEmotion("")
+        setCorrectEmotion("")
+        setThirdEmotion("")
         const gameCode = sessionStorage.getItem('game-code')
         sessionStorage.removeItem('is-disabled')
         sessionStorage.removeItem('is-time-over')
@@ -299,7 +303,7 @@ const game = () => {
                     <div className="font-bold px-8 py-4 heading rounded-xl w-3/4 text-lg">
                         Scene: {scene.scene}
                     </div>
-                    {player.isRandomlySelected && player.name === playerName? <Wheel emotionFunction = {guessEmotion} currentRoundEmotion={currentRoundEmotion} /> : <Wheel emotionFunction = {guessEmotion} deletedRow={deletedRow}/> }
+                    {player.isRandomlySelected && player.name === playerName? <Wheel emotionFunction = {guessEmotion} currentRoundEmotion={currentRoundEmotion} /> : <Wheel emotionFunction = {guessEmotion} deletedRow={deletedRow} callRobot={[correctEmotion, otherEmotion, thirdEmotion]}/> }
                 </div>
                 <div className="flex flex-column mx-2 flex-1" style={{height:"80vh"}}>
                     <div className="font-bold flex p-2 heading rounded-lg text-lg">
